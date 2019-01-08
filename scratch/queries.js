899033d2a2de5e2e2e7726993f8f5b4f36bd9b75
Marwan Alaloosi <manoalosi@gmail.com>
2019-01-08 15:06:25 -0600

    added delete note by id in scratch file

---
+++ scratch/queries.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const searchTerm = 'lady gaga';
+//     let filter = {};
+//     if (searchTerm) {
+//       filter.title = { $regex: searchTerm, $options: 'i' };
+//     }
+
+//     return Note.find(filter).sort({ updatedAt: 'desc' });
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+// Find by id
+
+//   mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
+//   .then(()=>{
+//       return Note.find();
+//     })
+//   .then(results=>{
+//       const id = results[0]._id;
+//       return Note.findById(id)
+//   })
+//   .then(results=>{
+//       console.log(results);
+//   })
+//   .then(()=>{
+//       return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+// create
+
+// mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
+// .then(()=>{
+//     const newNote = {
+//         title: "scratch title",
+//         content: "contents"
+//     }
+
+//     return Note.create(newItem)
+// })
+// .then(results=>{
+//     console.log(results)
+// })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+// update
+
+// mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
+// .then(()=>{
+//     return Note.find();
+// })
+// .then(results=>{
+//     const id = results[0]._id;
+//     const updatedNote = {
+//         title: "updated",
+//         content: "updated content"
+//     };
+//     return Note.findByIdAndUpdate(id,updatedNote);
+// })
+// .then(results=>{
+//     console.log(results)
+// })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+// Delete
+
+// mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
+// .then(()=>{
+//     return Note.find();
+// })
+// .then(results=>{
+//     const id = results[0]._id;
+//     return Note.findByIdAndRemove(id);
+// })
+// .then(results=>{
+//     console.log(results)
+// })
+// .catch(err=>{
+//     console.error(err);
+// });
+
