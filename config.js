eff99543cafc56e25828d505843118b24dd23190
Marwan Alaloosi <manoalosi@gmail.com>
2019-01-08 13:47:37 -0600

    config.js updated and added noteSchema with title and content, moduled and exported

--- config.js
+++ config.js
@@ -3,3 +3,8 @@
 module.exports = {
   PORT: process.env.PORT || 8080
 };
+
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful'
+};
